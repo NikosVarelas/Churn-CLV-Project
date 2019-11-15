69bc3f5db53a9c4079566361ac08a9e6930bd233
Nikos Varelas <nikolaos.varelas@ynap.com>
2019-11-15 10:29:34 +0000

    added the pipeline class

--- main_churn.py
+++ main_churn.py
@@ -1,19 +1,19 @@
-from utils import *
+from pipeline import Pipeline
 
-if __name__ == "__main__":
+
+import pandas as pd
+
+if __name__ == '__main__':
     print("Reading Data")
     data = pd.read_csv('./data/customerData.csv', sep=',')
     data['Transaction_date'] = pd.to_datetime(data['Transaction_date'])
+    pipeline = Pipeline(data=data,
+                        months=1,
+                        date_col='Transaction_date',
+                        basket_col='Basket_id',
+                        churn_days=14,
+                        lag=1
+                        )
 
-    # Predicting CLV one month ahead
 
-    print("Started preproc")
-    preprocess_train = Preprocessor(data, 1, 'Transaction_date', 'Basket_id', 14)
-    preprocess_labels = LabelEstimator(data, 1, 'Transaction_date', 'Basket_id', 14)
-    train_set, predict_set = preprocess_train.fit('Customer_no', 'item_net_amount')
-    labels = preprocess_labels.fit('Customer_no', 'item_net_amount')
 
-    # Dropping customers with less than 5 events
-    print(train_set.head())
-    print(predict_set.head())
-    print(labels.head())
