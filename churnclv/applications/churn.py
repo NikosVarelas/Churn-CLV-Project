0d4a05bae879f7f71b3acb25f32f0f496556345b
Dionysios Varelas <dionvarelas@gmail.com>
2019-11-16 12:58:58 +0000

    Added main

--- churnclv/applications/churn.py
+++ churnclv/applications/churn.py
@@ -1,7 +1,8 @@
 from pipeline import Pipeline
 import pandas as pd
 
-if __name__ == '__main__':
+
+def main():
     print("Reading Data")
     data = pd.read_csv('./data/customerData.csv', sep=',')
     data['Transaction_date'] = pd.to_datetime(data['Transaction_date'])
@@ -10,8 +11,12 @@ if __name__ == '__main__':
                         date_col='Transaction_date',
                         basket_col='Basket_id',
                         churn_days=14,
-                        lag=1
-                        )
+                        lag=1)
+    help(pipeline)
+
+
+if __name__ == '__main__':
+    main()
 
 
 
