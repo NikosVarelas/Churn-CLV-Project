d5d9bd4e263803712423c95b68ce642f99708e9a
Nikos Varelas <nikos.vare@gmail.com>
2019-11-22 15:45:15 +0000

    added compute tables method left join and join tables method

--- pipeline.py
+++ pipeline.py
@@ -17,5 +17,39 @@ class Pipeline(LabelCalculator, LaggedFeatures, PcaModel, Normalization):
                                  churn_days)
         PcaModel.__init__(self, n_components)
         self.normalisation = normalisation
+        self.train_df = None
+        self.predict_df = None
+        self.lagged_train = None
+        self.lagged_predict = None
+        self.labels = None
+
+    def compute_tables(self, key, value):
+        self.train_df, self.predict_df = super(LaggedFeatures, self).transform(key, value)
+        self.lagged_train, self.lagged_predict = LaggedFeatures.transform(self, key, value)
+        self.labels = LabelCalculator.transform(key, value)
+        return self
+
+    @staticmethod
+    def left_join(left, right, key):
+        joined_table = left.merge(right, on=key)
+        output = joined_table.fillna(0)
+
+        return output
+
+    def join_tables(self, key, value):
+        self.compute_tables(key, value)
+        train = self.left_join(self.train_df, self.lagged_train, key)
+        train_set = self.left_join(train, self.lagged_train, key)
+        predict_set = self.left_join(self.predict_df, key)
+
+        return train_set, predict_set
+
+
+
+
+
+
+
+
 
 
