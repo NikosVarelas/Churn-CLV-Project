d34b775a16bbf33eb015e7f85209ef9782c4a2c7
Nikos Varelas <nikos.vare@gmail.com>
2019-11-22 16:20:04 +0000

    fixed the bug on join_tables method

--- pipeline.py
+++ pipeline.py
@@ -26,7 +26,7 @@ class Pipeline(LabelCalculator, LaggedFeatures, PcaModel, Normalization):
     def compute_tables(self, key, value):
         self.train_df, self.predict_df = super(LaggedFeatures, self).transform(key, value)
         self.lagged_train, self.lagged_predict = LaggedFeatures.transform(self, key, value)
-        self.labels = LabelCalculator.transform(key, value)
+        self.labels = LabelCalculator.transform(self, key, value)
         return self
 
     @staticmethod
@@ -39,8 +39,8 @@ class Pipeline(LabelCalculator, LaggedFeatures, PcaModel, Normalization):
     def join_tables(self, key, value):
         self.compute_tables(key, value)
         train = self.left_join(self.train_df, self.lagged_train, key)
-        train_set = self.left_join(train, self.lagged_train, key)
-        predict_set = self.left_join(self.predict_df, key)
+        train_set = self.left_join(train, self.labels, key)
+        predict_set = self.left_join(self.predict_df, self.lagged_predict, key)
 
         return train_set, predict_set
 
