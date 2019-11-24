d571d81d39ab59e018bed05d68bdb76de0ee5d34
NikosVarelas <nikos.vare@gmail.com>
2019-11-24 10:25:17 +0000

    added pipeline transform methond on main

--- churnclv/applications/churn.py
+++ churnclv/applications/churn.py
@@ -1,5 +1,6 @@
 from pipeline import Pipeline
 import pandas as pd
+from churnclv.utils.feature_engineering import train_valid_test_split
 
 
 def main():
@@ -12,11 +13,11 @@ def main():
                         basket_col='Basket_id',
                         churn_days=14,
                         lag=1)
-    help(pipeline)
+    pipeline.fit('Customer_no', 'item_net_amount')
+    train, predict = pipeline.transform('Customer_no', 'item_net_amount')
+    x_train, x_val, x_test, y_train, y_val, y_test = train_valid_test_split(
+        train, 'churn', 0.3, stratify_fold=True)
 
 
 if __name__ == '__main__':
     main()
-
-
-
