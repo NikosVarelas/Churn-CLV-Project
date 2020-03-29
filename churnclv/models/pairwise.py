73fb402cfe72cf9527d25e2357acb49785a47dca
NikosVarelas <nikos.vare@gmail.com>
2020-03-29 17:31:48 +0100

    added the pairwise model

--- churnclv/models/pairwise.py
+++ churnclv/models/pairwise.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 
 
 class PairwiseModel(object):
-
     def __init__(self, input_shape):
         self.input_shape = input_shape
 
@@ -13,14 +12,24 @@ class PairwiseModel(object):
 
     def create_base_network(self):
         input_features = tf.keras.Input(shape=self.input_shape)
-        x_hidden = tf.keras.layers.Dense(
-            4,
-            kernel_regularizer=tf.keras.regularizers.l2(0.01),
-            activation='sigmoid')(input_features)
-
         x = tf.keras.layers.Dense(
             1,
-            kernel_regularizer=tf.keras.regularizers.l2(0.001),
-            activation='sigmoid')(x_hidden)
+            kernel_regularizer=tf.keras.regularizers.l2(0.01),
+            activation='sigmoid')(input_features)
         return tf.keras.models.Model(input_features, x)
 
+    def siamese_network(self):
+        positive_features = tf.keras.Input(shape=self.input_shape, name='positive_customer')
+        negative_features = tf.keras.Input(shape=self.input_shape, name='negative_customer')
+
+        base_scorer = self.create_base_network()
+        positive_score = base_scorer(positive_features)
+        negative_score = base_scorer(negative_features)
+
+        score_diff = tf.keras.layers.Lambda(
+            lambda x: x[0]-x[1],
+            name='score_diff'
+        )([positive_score, negative_score])
+
+        return tf.keras.models.Model(input=[positive_features, negative_features],
+                                     output=score_diff)
