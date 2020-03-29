47e6804d163a537485d0078c988d98954844834f
NikosVarelas <nikos.vare@gmail.com>
2020-03-29 20:02:09 +0100

    removed the pointwise model from the churn module

--- churnclv/applications/churn.py
+++ churnclv/applications/churn.py
@@ -1,9 +1,11 @@
+import numpy as np
 import pickle
 import os
 import tensorflow as tf
 
 from churnclv import BASE_PATH
 from churnclv.models.pairwise import PairwiseModel
+from churnclv.models.pairwise import create_pairs
 
 
 def main():
@@ -11,19 +13,32 @@ def main():
     with open(BASE_PATH + '/output/datasets.pickle', 'rb') as handle:
         data = pickle.load(handle)
 
-    model = PairwiseModel(input_shape=8).create_base_network()
-    model.compile(loss='binary_crossentropy',
-                  optimizer='sgd',
-                  metrics=['accuracy', tf.keras.metrics.AUC(name='auc')])
+    pairwise = PairwiseModel(input_shape=8)
 
-    model.fit(data['x_train_churn'].values,
-              data['y_train_churn'].values,
-              validation_data=(data['x_val_churn'].values, data['y_val_churn'].values),
-              epochs=500)
+    # Create the training set
+    positive_customer, negative_customer, label_train = create_pairs(
+        data['x_train_churn'].values, data['y_train_churn'].values)
+
+    # Create the validation set
+    positive_val, negative_val, label_val = create_pairs(
+        data['x_val_churn'].values, data['y_val_churn'].values)
+
+    siamese = pairwise.siamese_network()
+    siamese.compile(loss='binary_crossentropy',
+                    optimizer='sgd',
+                    metrics=['accuracy'])
+
+    print(siamese.summary())
+
+    siamese.fit(
+        [positive_customer, negative_customer],
+        label_train,
+        validation_data=([positive_val, negative_val], label_val),
+        epochs=500)
 
     if not os.path.isdir(BASE_PATH + '/trained_models'):
         os.mkdir(BASE_PATH + '/trained_models')
-    model.save(BASE_PATH + '/trained_models/' + 'pointwise_model.h5')
+    siamese.save(BASE_PATH + '/trained_models/' + 'siamese_model.h5')
 
 
 if __name__ == '__main__':
