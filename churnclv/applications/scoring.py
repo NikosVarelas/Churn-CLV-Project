cf1b0d060576916b06994ed8365f8de1b6383a48
NikosVarelas <nikos.vare@gmail.com>
2020-04-05 17:08:20 +0100

    added the score of the pairwise model

--- churnclv/applications/scoring.py
+++ churnclv/applications/scoring.py
@@ -12,10 +12,20 @@ def main():
 
     model = tf.keras.models.load_model(BASE_PATH + '/trained_models/pointwise_model.h5')
 
-    preds = model.predict(data['x_test_churn'])
-    y_pred = [x[0] for x in preds]
-    y_true = data['y_test_churn']
-    print('AUC on test data: ' + str(roc_auc_score(y_true, y_pred)))
+    preds = model.predict(data['x_test_churn'].values)
+    y_pred_lr = [x[0] for x in preds]
+    y_true = data['y_test_churn'].values
+
+    siamese = tf.keras.models.load_model(BASE_PATH + '/trained_models/siamese_model.h5', custom_objects={'tf': tf})
+    print(siamese.summary())
+
+    base_scorer = siamese.get_layer('model')
+
+    preds = base_scorer.predict(data['x_test_churn'].values)
+    y_pred_siam = [x[0] for x in preds]
+
+    print('AUC on test data: ' + str(roc_auc_score(y_true, y_pred_lr)))
+    print('AUC on test data: ' + str(roc_auc_score(y_true, y_pred_siam)))
 
 
 if __name__ == '__main__':
